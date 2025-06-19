ff390be95fe5bb314177363aabe2f94bf5eb5dfe
manellylopez <manellylopezqqq@gmail.com>
2025-06-18 20:29:25 -0500

    enable text compression with vite plugin

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import viteCompression from "vite-plugin-compression";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteCompression({
+      verbose: true,
+      disable: false,
+      algorithm: "brotliCompress",
+      ext: ".br",
+    }),
+  ],
   server: {
     headers: {
       'Cache-Control': 'max-age=604800',
